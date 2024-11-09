0ae4ad7bf134ac1f1ef32e71bde3635c726b1bd1
carlosgonzalez <carlosjgr7@gmail.com>
2024-11-09 14:56:10 -0400

    fix: fix loginViewModelTest

--- app/src/test/java/com/delivery/tecnokargo/app/viewModels/LoginViewModelTest.kt
+++ app/src/test/java/com/delivery/tecnokargo/app/viewModels/LoginViewModelTest.kt
@@ -6,11 +6,15 @@ import com.delivery.tecnokargo.login.presentation.model.LoginPresentationData
 import com.delivery.tecnokargo.login.presentation.viewmodels.LoginViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -24,24 +28,28 @@ class LoginViewModelTest {
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
-    private val loginUseCase: LoginUseCase = Mockito.mock(LoginUseCase::class.java)
+
     private lateinit var loginViewModel: LoginViewModel
+    private lateinit var mockLoginUseCase: LoginUseCase
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 
     @Before
     fun setUp() {
-        loginViewModel = LoginViewModel(dispatcher, loginUseCase)
+        mockLoginUseCase = Mockito.mock(LoginUseCase::class.java)
+        loginViewModel = LoginViewModel(dispatcher, mockLoginUseCase)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `login with valid credentials should update coroutine correctly`() = runTest {
         val username = "user"
         val password = "pass"
         val loginResponse = LoginPresentationData(name = "Login correcto")
-        val expectedPresentationData =
-            Resources.Success(LoginPresentationData(name = "Login correcto"))
+        val expectedPresentationData = Resources.Success(loginResponse)
         val data: List<String> = listOf(CONSTANTS.NUMBERDB, username, password)
-        whenever(loginUseCase.invoke(data)).thenReturn(flowOf(Result.success(loginResponse)))
+
+        // Setup the behavior of the mocked use case
+        whenever(mockLoginUseCase.invoke(data)).thenReturn(flowOf(Result.success(loginResponse)))
 
         var emissionCount = 0
         val loginStateFlow = loginViewModel.loginState
@@ -50,7 +58,7 @@ class LoginViewModelTest {
         loginStateFlow.take(2).collect { result ->
             when (emissionCount) {
                 0 -> {
-                    Assert.assertTrue(result is Resources.Loading)
+                    assertTrue(result is Resources.Loading)
                 }
                 1 -> {
                     assertEquals(expectedPresentationData, result)
